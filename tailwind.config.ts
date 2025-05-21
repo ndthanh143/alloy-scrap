25a2097134939eb1bb0ac5ac93edb408d89ea67f
ndthanh143 <nguyenduythanh421.tna@gmail.com>
2025-05-21 14:55:22 +0700

    fix color shacn/iui

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// export default {
+//   darkMode: ["class"], // <-- rất quan trọng cho theme dark/light
+//   theme: {
+//     extend: {
+//       colors: {
+//         // Không cần override thủ công nếu dùng default từ shadcn
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// };
