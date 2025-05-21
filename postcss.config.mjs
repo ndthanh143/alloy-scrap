ca3be6d3f8313c1a314f8306a1985f0cdba99d51
ndthanh143 <nguyenduythanh421.tna@gmail.com>
2025-05-21 15:08:31 +0700

    add color func

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,7 @@
+const postcssOKLabFunction = require("@csstools/postcss-oklab-function");
+
 const config = {
+  "postcss-oklab-function": postcssOKLabFunction,
   plugins: ["@tailwindcss/postcss"],
 };
 
