6ad10680e6e4d9dfa37854eeae5f51199d23244a
ndthanh143 <nguyenduythanh421.tna@gmail.com>
2025-06-03 19:20:13 +0700

    Revalidate 2

--- src/app/bai-viet/page.tsx
+++ src/app/bai-viet/page.tsx
@@ -4,7 +4,7 @@ import { formatDate } from "@/lib/formatDate";
 import Image from "next/image";
 import { BreadCrumb } from "@/components";
 
-export const revalidate = process.env.REVALIDATE_TIME_BLOGS || 60;
+export const revalidate = Number(process.env.REVALIDATE_TIME_BLOGS) || 3600;
 
 export default async function BlogsPage() {
   const posts = await getPublishedPosts();
