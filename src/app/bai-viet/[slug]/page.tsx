3456e1977993b578f8dd1f35d9787f6c6c2eaf07
ndthanh143 <nguyenduythanh421.tna@gmail.com>
2025-06-24 23:09:08 +0700

    Metadata

--- src/app/bai-viet/[slug]/page.tsx
+++ src/app/bai-viet/[slug]/page.tsx
@@ -5,10 +5,57 @@ import { getPostBySlug } from "@/lib/notion";
 import { Content } from "./_components/Content";
 import { BreadCrumb } from "@/components";
 import Image from "next/image";
+import { Metadata } from "next";
 
 const notionApi = new NotionAPI();
 
 export const revalidate = 60; // Revalidate every 60 seconds
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const page = await getPostBySlug(params.slug);
+
+  if (!page) return {};
+
+  const title =
+    (page as any).properties.Name.title[0]?.plain_text || "Không tiêu đề";
+
+  const description =
+    (page as any).properties.Summary?.rich_text[0]?.plain_text ||
+    "Cập nhật tin tức mới nhất về thu mua phế liệu hợp kim.";
+
+  const thumbnail =
+    (page as any).properties.Thumbnail.files[0]?.file?.url ||
+    "/placeholder.png";
+
+  const fullUrl = `${process.env.NEXT_PUBLIC_CLIENT_URL}/bai-viet/${params.slug}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: fullUrl,
+      images: [
+        {
+          url: thumbnail,
+          width: 800,
+          height: 400,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [thumbnail],
+    },
+  };
+}
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default async function BlogPost({ params }: { params: any }) {
